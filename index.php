4096dce890a797443a0ff96c18e179a507ddc619
ryanderton <ryanderton@gmail.com>
2016-03-05 10:25:05 -0800

    Added files via upload
    Logo link

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $(document).ready(function(){
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Ryanderton</a>
+			<a class="navbar-brand" href="http://www.ryanderton.com">Ryanderton</a>
 		</div>
 
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
